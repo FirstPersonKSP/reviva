bef7027171549b220974420bcba047c827f58a84
Harvey Thompson <harv.thompson@icloud.com>
2022-01-29 14:04:06 -0800

    Tidy up code and improve part switch config.

--- plugin/IVASwitchPart.cs
+++ plugin/IVASwitchPart.cs
@@ -13,30 +13,49 @@ namespace Reviva
 
 	public override void OnLoad(ConfigNode node)
 	{
-            string newName = (internalName != null ? internalName : "null");
-            Debug.Log("IVASwitch: internalName=" + newName);
-
             base.OnLoad(node);
 	    IVASwitch();
 	}
 
 	private void IVASwitch()
 	{
-	    if (string.IsNullOrEmpty(internalName))
-		return;
+            string oldName = GetCurrentInternalName();
+            string newName = GetRequiredInternalName();
 
-	    ConfigNode oldInternalConfig = part.partInfo.internalConfig;
-	    string oldName = (oldInternalConfig.HasValue("name")
-			      ? oldInternalConfig.GetValue("name")
-			      : null);
-	    if (string.IsNullOrEmpty(oldName))
-                oldName = "null";
-
-            Debug.Log("IVASwitch: " + oldName + " -> " + internalName);
-
+            Debug.Log($"IVASwitch: {oldName} -> {newName}");
+            if (newName == "")
+            {
+                Debug.Log("IVASwitch: internalName is null or empty, no switch");
+                return;
+            }
+	    if (oldName == newName)
+            {
+                Debug.Log("IVASwitch: internalName unchanged, no switch");
+                return;
+            }
+    
 	    ConfigNode newInternalConfig = new ConfigNode("INTERNAL");
-	    newInternalConfig.AddValue("name", internalName);
-	    part.partInfo.internalConfig = newInternalConfig;
+	    newInternalConfig.AddValue("name", newName);
+	    this.part.partInfo.internalConfig = newInternalConfig;
 	}
+
+        private string GetCurrentInternalName()
+        {
+	    ConfigNode internalConfig = this.part.partInfo.internalConfig;
+	    string name = (internalConfig.HasValue("name")
+			   ? internalConfig.GetValue("name")
+			   : null);
+	    if (name == null)
+                name = "";
+            return name;
+        }
+
+        private string GetRequiredInternalName()
+        {
+            string name = this.internalName;
+	    if (name == null)
+                name = "";
+            return name;
+        }
     }
 }
