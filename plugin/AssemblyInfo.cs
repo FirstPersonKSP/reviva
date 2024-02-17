06c18a462dc559e3bf1041eeaaa218ab92efbd87
Harvey Thompson <harv.thompson@icloud.com>
2024-02-17 13:19:47 -0800

    Added AssemblyInfo.cs and Reviva.csproj.user-example
    - Eventually these probably will be automatically updated and/or generated?

---
+++ plugin/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Reviva")]
+[assembly: AssemblyDescription("KSP IVA Revival")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("610yesnolovely")]
+[assembly: AssemblyProduct("Reviva")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e33468f4-ccae-4af4-ab17-0f48195d85c7")]
+
+[assembly: AssemblyInformationalVersionAttribute("1.0.0-1-g6994e37")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+
+[assembly: KSPAssembly("Reviva", 1, 0, 0)]
