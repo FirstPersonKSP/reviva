c85181bd876236381856f4c1989913f9a0d4ed4a
Harvey Thompson <harv.thompson@icloud.com>
2024-02-17 14:57:57 -0800

    Reindent using spaces, according to editorconfig.

--- plugin/MASComputer.cs
+++ plugin/MASComputer.cs
@@ -5,76 +5,76 @@ using UnityEngine;
 
 namespace Reviva
 {
-	public class MASComputer : BaseComputer
-	{
-		public MASComputer(ModuleIVASwitch ivaSwitch)
-			: base(ivaSwitch)
-		{
-		}
+    public class MASComputer : BaseComputer
+    {
+        public MASComputer(ModuleIVASwitch ivaSwitch)
+            : base(ivaSwitch)
+        {
+        }
 
-		public override bool IsEnabled => masEnabled;
+        public override bool IsEnabled => masEnabled;
 
-		public override string ModuleName => "MASFlightComputer";
+        public override string ModuleName => "MASFlightComputer";
 
-		/* ------------------------------------------------------------------------------- */
+        /* ------------------------------------------------------------------------------- */
 
-		protected override ConfigNode CreateDefaultData()
-		{
-			// If there is no MASFlightComputer data, the default is empty unless RPM is also
-			// present in which case the best option is more likely some reasonable defaults
-			// copied from AvionicsSystems/GameData/MOARdV/Patches/JsiToMasUpgrade.cfg
-			return ConfigNode.Parse(@"
-				requiresPower = true
-				gLimit = 4.7
-				baseDisruptionChance = 0.20
-				// stored strings are part of the ALCOR lander pod, but they may be referenced elsewhere.
-				PERSISTENT_VARIABLES
-				{
-					storedStrings0=v 0.9.7.0
-					storedStrings1=08.07.2020
-					storedStrings2=A.L.C.O.R.
-					storedStrings3=ADVANCED LANDING CAPSULE
-					storedStrings4=FOR    ORBITAL    RENDEZVOUS
-					storedStrings5=ALC00236-A3
-					storedStrings6=ALCOR
-					storedStrings7=A.S.E.T.
-				}");
-		}
+        protected override ConfigNode CreateDefaultData()
+        {
+            // If there is no MASFlightComputer data, the default is empty unless RPM is also
+            // present in which case the best option is more likely some reasonable defaults
+            // copied from AvionicsSystems/GameData/MOARdV/Patches/JsiToMasUpgrade.cfg
+            return ConfigNode.Parse(@"
+                requiresPower = true
+                gLimit = 4.7
+                baseDisruptionChance = 0.20
+                // stored strings are part of the ALCOR lander pod, but they may be referenced elsewhere.
+                PERSISTENT_VARIABLES
+                {
+                    storedStrings0=v 0.9.7.0
+                    storedStrings1=08.07.2020
+                    storedStrings2=A.L.C.O.R.
+                    storedStrings3=ADVANCED LANDING CAPSULE
+                    storedStrings4=FOR    ORBITAL    RENDEZVOUS
+                    storedStrings5=ALC00236-A3
+                    storedStrings6=ALCOR
+                    storedStrings7=A.S.E.T.
+                }");
+        }
 
-		// MASFlightComputer does not use OnLoad() to get PERSISTENT_VARIABLES,
-		// RPM_COLOROVERRIDE, or MAS_ACTION_GROUP.  These have to be updated in the
-		// part.partInfo - which is sort of sketchy, but essentially Reviva is taking
-		// control of INTERNAL anyway, so now also MASFlightComputer config.
-		protected override void RecreateComputerPreAddHook(Part ownerPart, ConfigNode newConfig)
-		{
-			base.RecreateComputerPreAddHook(ownerPart, newConfig);
+        // MASFlightComputer does not use OnLoad() to get PERSISTENT_VARIABLES,
+        // RPM_COLOROVERRIDE, or MAS_ACTION_GROUP.  These have to be updated in the
+        // part.partInfo - which is sort of sketchy, but essentially Reviva is taking
+        // control of INTERNAL anyway, so now also MASFlightComputer config.
+        protected override void RecreateComputerPreAddHook(Part ownerPart, ConfigNode newConfig)
+        {
+            base.RecreateComputerPreAddHook(ownerPart, newConfig);
 
-			// Find the existing FlightComputer in partInfo and change it to the new one.
-			ConfigNode[] oldComputerNodes = ownerPart.partInfo.partConfig.GetNodes("MODULE", "name", this.ModuleName);
-			foreach (ConfigNode oldComputerNode in oldComputerNodes)
-			{
+            // Find the existing FlightComputer in partInfo and change it to the new one.
+            ConfigNode[] oldComputerNodes = ownerPart.partInfo.partConfig.GetNodes("MODULE", "name", this.ModuleName);
+            foreach (ConfigNode oldComputerNode in oldComputerNodes)
+            {
 #if REVIVA_DEBUG
                 Log($"Removing: {oldComputerNode}");
 #endif
-				ownerPart.partInfo.partConfig.RemoveNode(oldComputerNode);
-			}
+                ownerPart.partInfo.partConfig.RemoveNode(oldComputerNode);
+            }
 
-			ConfigNode newComputerNode = new ConfigNode("MODULE");
-			newConfig.CopyTo(newComputerNode);
+            ConfigNode newComputerNode = new ConfigNode("MODULE");
+            newConfig.CopyTo(newComputerNode);
 
 #if REVIVA_DEBUG
-	    Log($"Adding: {newComputerNode}");
+        Log($"Adding: {newComputerNode}");
 #endif
-			ownerPart.partInfo.partConfig.AddNode(newComputerNode);
-		}
+            ownerPart.partInfo.partConfig.AddNode(newComputerNode);
+        }
 
-		/* ------------------------------------------------------------------------------- */
-		private static readonly bool masEnabled;
-		private static readonly string masAssembly = "AvionicsSystems";
+        /* ------------------------------------------------------------------------------- */
+        private static readonly bool masEnabled;
+        private static readonly string masAssembly = "AvionicsSystems";
 
-		static MASComputer()
-		{
-			masEnabled = BaseComputer.DetectAssembly(masAssembly);
-		}
-	}
+        static MASComputer()
+        {
+            masEnabled = BaseComputer.DetectAssembly(masAssembly);
+        }
+    }
 }
--- plugin/RPMComputer.cs
+++ plugin/RPMComputer.cs
@@ -5,30 +5,30 @@ using UnityEngine;
 
 namespace Reviva
 {
-	public class RPMComputer : BaseComputer
-	{
-		public RPMComputer(ModuleIVASwitch ivaSwitch)
-			: base(ivaSwitch)
-		{
-		}
+    public class RPMComputer : BaseComputer
+    {
+        public RPMComputer(ModuleIVASwitch ivaSwitch)
+            : base(ivaSwitch)
+        {
+        }
 
-		public override bool IsEnabled => rpmEnabled;
-		public override string ModuleName => "RasterPropMonitorComputer";
+        public override bool IsEnabled => rpmEnabled;
+        public override string ModuleName => "RasterPropMonitorComputer";
 
-		/* ------------------------------------------------------------------------------- */
+        /* ------------------------------------------------------------------------------- */
 
-		protected override ConfigNode CreateDefaultData()
-		{
-			return new ConfigNode();
-		}
+        protected override ConfigNode CreateDefaultData()
+        {
+            return new ConfigNode();
+        }
 
-		/* ------------------------------------------------------------------------------- */
-		private static readonly bool rpmEnabled;
-		private static readonly string rpmAssembly = "RasterPropMonitor";
+        /* ------------------------------------------------------------------------------- */
+        private static readonly bool rpmEnabled;
+        private static readonly string rpmAssembly = "RasterPropMonitor";
 
-		static RPMComputer()
-		{
-			rpmEnabled = BaseComputer.DetectAssembly(rpmAssembly);
-		}
-	}
+        static RPMComputer()
+        {
+            rpmEnabled = BaseComputer.DetectAssembly(rpmAssembly);
+        }
+    }
 }
